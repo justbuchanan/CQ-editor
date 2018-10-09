f55f576410edd9a7f001a7a56d4add304eb1b827
Justin Buchanan <justbuchanan@gmail.com>
2018-10-09 00:26:54 -0700

    example of showing a cqparts object

---
+++ cqparts_example.py
@@ -0,0 +1,18 @@
+import cadquery as cq
+import cqparts
+import car
+
+def show_cqparts_obj(cqobj):
+    cqobj.build(recursive=True)
+    def _show_recursive(cqobj, prefix=''):
+        if isinstance(cqobj, cqparts.Part):
+            # cq-editor detects cadquery objects in the global namespace
+            globals()[prefix] = cqobj.world_obj
+        elif isinstance(cqobj, cqparts.Assembly):
+            if prefix: prefix = prefix + "_"
+            for name, subobj in cqobj.components.items():
+                _show_recursive(subobj, prefix + name)
+    _show_recursive(cqobj)
+
+
+show_cqparts_obj(car.Car())
